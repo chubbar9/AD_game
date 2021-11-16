323c893c52f36f2be032859b2686b0bc3702bba2
chubbar9 <charleyjhubbard@gmail.com>
2021-11-15 19:50:23 -0500

    create initial commit to repo

---
+++ AD_game_start_script.R
@@ -0,0 +1,5 @@
+#load game functions
+source('c:/Users/charl/OneDrive/Desktop/AD_Game/AD_game_functions.R')
+
+#create coaches df
+coaches_out_df = create_coaches_df(static_dir_path, seed, number_of_teams)
